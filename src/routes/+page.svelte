0b1ef9859d6fd25c7e0e72c80008fb2e66adc6c9
Doodle <86998020+Goofydoodle@users.noreply.github.com>
2023-12-25 13:00:19 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,125 @@
+<script>
+
+
+</script>
+
+<div class="name">
+ <h1>Gagan</h1>
+</div>
+
+<div class="sabout">
+    <h2>Code enthusiast & student</h2>
+</div>
+
+<div class="about">
+ <p>Hiya! I'm a high school coder, sharpening my coding <br>skills in Python🐍 and SvelteKit⚡</p>
+</div>
+
+<div class="projects">
+ <h3>+projects</h3>
+
+
+
+   <div class="comingsoon">
+    <h4>• coming soon</h4>
+   </div>
+</div>
+
+
+<div class="contact">
+<h3>+contact</h3>
+    <div class="github">
+        <p>Github: <a href="https://github.com/Goofydoodle" target="_blank" class="git">@Goofydoodle</a></p>
+    </div>
+    <div class="email">
+        <p>Email:<a href="mailto:mail.gagan.uk@gmail.com">mail.gagan.uk@gmail.com</a>
+    </div>
+</div>
+
+
+<style>
+.name{
+    position: relative;
+    text-align: center;
+    top: 150px;
+    right:200px;
+     
+    }
+
+.sabout{
+    position: relative;
+    text-align: center;
+    top: 130px;
+    right:113px;
+    font-size: 12px;
+    padding: 0px;
+    }
+.about{
+    position: relative;
+    text-align: center;
+    top: 120px;
+    right:20px;
+    font-size: 15px;
+    padding: 0px;
+    color:#a5a5a5;
+    }
+
+.projects{position: relative;
+    text-align: center;
+    top: 120px;
+    right:185px; 
+    font-size: 20px;
+    padding: 0px;
+    }
+.comingsoon{
+    position: relative;
+    text-align: center;
+    font-size:15px;
+    top: -15px;
+    right: 5px;
+    padding: 0px;
+    color: #a5a5a5;
+    }
+
+.contact{
+    position: relative;
+    text-align: center;
+    top: 110px;
+    right: 10px;
+    left: -192px;
+    font-size: 20px;
+    padding: 0px;
+    }
+
+.github{
+    position: relative; 
+    text-align: center;
+    top: -15px;
+    right:0px;
+    left: 33px;
+    padding: 0px;
+    font-size:15px;
+    }
+.email{
+    position: relative;
+    text-align: center; 
+    top: -15px;
+    right: 0px;
+    left: 73px;
+    padding: 0px;
+    font-size:15px;
+    
+    }
+
+
+ a:hover{
+    color: rgb(75, 172, 75);
+    text-decoration: none;
+    }
+
+    a{
+        color: rgb(165, 165, 165);
+        text-decoration: none;
+    }
+
+</style>    
