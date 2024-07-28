78355444376148b71b89f1a914b4f9f4e7f3bd07
Doodle <86998020+Goofydoodle@users.noreply.github.com>
2024-07-28 20:57:11 +0530

    Better-CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,138 +1,68 @@
 <script>
-
-
+  
 </script>
 
-<div class="name">
- <h1>Gagan</h1>
-</div>
+<div class="main">
+ <div class="content">
 
-<div class="sabout">
+   <div class="name">
+   <h1>Gagan</h1>
+   </div>
+
+   <div class="sabout">
     <h2>Code enthusiast & student</h2>
-</div>
+   </div>
 
-<div class="about">
- <p>Hiya! I'm a high school coder, sharpening my coding <br>skills in Python🐍 and SvelteKit⚡</p>
-</div>
+   <div class="about">
+   <p>Hiya! I'm a high school coder, sharpening my coding skills in Python🐍 and SvelteKit⚡</p>
+   </div>
 
-<div class="projects">
- <h3>+projects</h3>
+   <div class="projects">
+   <h3>+projects</h3>
 
 
-  <div class="QR-Gen">
+   <div class="QR-Gen">
     <h4><a href="https://github.com/Goofydoodle/QR-Gen" target="_blank">•QR-Gen</a></h4> 
-  </div>
-  <div class="QR-Gen-desc">
+   </div>
+  
+   <div class="QR-Gen-desc">
     <p>QR-Gen is a QR code generator made with Python.</p>
+   </div>
+
+
   </div>
 
 
-</div>
-
-
-<div class="contact">
-<h3>+contact</h3>
+   <div class="contact">
+   <h3>+contact</h3>
     <div class="github">
         <p>Github: <a href="https://github.com/Goofydoodle" target="_blank" class="git">@Goofydoodle</a></p>
     </div>
     <div class="email">
-        <p>Email:<a href="mailto:mail.gagan.uk@gmail.com">mail.gagan.uk@gmail.com</a>
+        <p>Email:<a href="mailto:contact@gaganuk.tech">contact@gaganuk.tech</a>
     </div>
 </div>
+</div>
 
-
+</div>
 <style>
-.name{
-    position: relative;
-    text-align: center;
-    top: 150px;
-    right:200px;
-    padding:0px;
-    
-    }
 
-.sabout{
-    position: relative;
-    text-align: center;
-    top: 130px;
-    right:113px;
-    font-size: 12px;
-    padding: 0px;
-    
-    }
-.about{
-    position: relative;
-    text-align: center;
-    top: 120px;
-    right:20px;
-    font-size: 15px;
-    padding: 0px;
-    color:#a5a5a5;
-    
-    }
+.main{
 
-.projects{position: relative;
-    text-align: center;
-    top: 120px;
-    right:185px; 
-    font-size: 20px;
-    padding: 0px;
-    
-    }
-.QR-Gen{
-    position: relative;
-    text-align: center;
-    font-size:17px;
-    top: -15px;
-    right: 25px;
-    padding: 0px;
-    color: #a5a5a5;
-    
-    }
+  text-align: center;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-.QR-Gen-desc{
-    position: relative;
-    text-align: center;
-    font-size: 14px;
-    top: -30px;
-    left: 138px;
-    padding: 0px;
-    color: #a5a5a5;
-    
-    }
+}
 
-.contact{
-    position: relative;
-    text-align: center;
-    top: 100px;
-    right: 10px;
-    left: -192px;
-    font-size: 20px;
-    padding: 0px;
-    
-    }
+.content {
+            max-width: 600px;
+            text-align: left; 
+        }
 
-.github{
-    position: relative; 
-    text-align: center;
-    top: -15px;
-    right:0px;
-    left: 33px;
-    padding: 0px;
-    font-size:15px;
-    
-    }
-.email{
-    position: relative;
-    text-align: center; 
-    top: -25px;
-    right: 0px;
-    left: 73px;
-    padding: 0px;
-    font-size:15px;
-    
-    
-    }
 
  a {
   background-image: linear-gradient(
@@ -148,6 +78,7 @@
   position: relative;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+  
   transition: all 0.3s ease-in-out;
  }
 
@@ -174,7 +105,9 @@
     @media only screen and (max-width: 600px) {
   .name, .sabout, .about, .projects, .QR-Gen, .QR-Gen-desc, .contact, .github, .email {
     position: relative;
-    text-align: center;
+    text-align: left;
+    justify-content: center;
+    align-items: center;
     top: 0;
     right: 0;
     left: 0;
